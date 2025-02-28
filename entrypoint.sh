24266253712b630c966ddcf656282ac21075143e
iizukar <czechia.deitan@gmail.com>
2025-02-28 23:19:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Start Honeygain
-./honeygain -tou-get
-./honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
+./pawns-cli \
+  -email="$PAWNS_EMAIL" \
+  -password="$PAWNS_PASSWORD" \
+  -device-name="$PAWNS_DEVICE_NAME" \
+  -device-id="$PAWNS_DEVICE_ID" \
+  -accept-tos &
 
 # Start dummy HTTP server on port 8000
 python3 -m http.server 8000 --bind 0.0.0.0 &
