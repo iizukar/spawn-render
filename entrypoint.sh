6d4161c54ef84866c6686025871af8a8727ea0d5
iizukar <czechia.deitan@gmail.com>
2025-02-28 15:11:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-# Use Render's injected PORT variable, default to 8000 if not set
-PORT=${PORT:-8000}
-
-# Start Honeygain (ensure variables are set in Render's environment)
+# Start Honeygain
 ./honeygain -tou-get
 ./honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
 
-# Start HTTP server on the assigned port
-python3 -m http.server $PORT --bind 0.0.0.0 &
+# Start dummy HTTP server on port 8000
+python3 -m http.server 8000 --bind 0.0.0.0 &
 
-# Keep the container running
+# Keep the container alive
 tail -f /dev/null
